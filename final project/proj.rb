f5cdfce4dc265262c0dca991cf135a33522498cf
katherineDeLaRosa <katherine.delarosa01@utrgv.edu>
2018-04-10 17:33:42 -0500

    a template of what we will be working with

---
+++ final project/proj.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+#require 'json'
+#require 'httparty'
+
+
+get '/' do
+	"sign in / sign up link"
+end
+get '/group' do 
+	"group code:"
+end
+get '/payment' do
+	"this is where we do pay things & checking about spotify here (if we get that far)"
+end
+post '/charge' do
+	"this is checking if vaild stripe would go here"
+end
