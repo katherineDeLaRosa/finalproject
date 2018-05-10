c3ccbeefa446c33082371bd397cf93be6fe2575e
katherine de la rosa <katherine.delarosa01@utrgv.edu>
2018-05-10 00:25:01 -0500

    changed gemfile for heroku

--- final project/controller.rb
+++ final project/controller.rb
@@ -17,6 +17,18 @@ def create
     :currency    => 'usd'
   )
 
+#this part is probably going to lead to an error
+#without keys i cannot alter
+
+#begin 
+# risky code
+#rescue 
+#error fix
+#sends an error message
+#end
+
+
+
 rescue Stripe::CardError => e
   flash[:error] = e.message
   redirect_to new_charge_path
